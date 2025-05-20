3d96c088d87750e2e01d305f23c8af898748c9d5
adaanaknya <124648170+adaanaknya@users.noreply.github.com>
2025-05-20 17:44:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,5 +29,10 @@ def start():
 def ui():
      st.title('Welcome')
 
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
 if __name__=='__main__':
     ui()
