916c2561028909556544037b1ceae3d214d61682
adaanaknya <124648170+adaanaknya@users.noreply.github.com>
2025-05-20 16:46:50 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,33 @@
+import main
+import connection
+import streamlit as st
+
+
+def start():
+    pilih = int(input("Input : "))
+    if pilih == 1:
+        many_rows = int(input("Berapa Data yang ingin di input "))
+        i = 1
+        while i <= many_rows:
+            name = input("input nama barang = ")
+            stock = int(input("input stock barang = "))
+            price = int(input("input harga beli (dalam rupiah)= "))
+            try:
+                connection.insert_barang(name,stock,price)
+            except:
+                print("Data sudah ada ")
+                break
+                
+            i = i+1
+        print("data berhasil di input")    
+        
+    elif pilih == 2:
+        nama_barang = input("pilih barang ")
+        print(connection.tampil_barang(nama_barang))
+ 
+ 
+def ui():
+     st.title('Welcome')
+
+if __name__=='__main__':
+    ui()
