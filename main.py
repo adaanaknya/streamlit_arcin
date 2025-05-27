833e33d9ebf17f3c464ef90198040192c8356062
adaanaknya <yogyog377@gmail.com>
2025-05-27 15:49:43 +0700

    dsa

--- main.py
+++ main.py
@@ -5,17 +5,9 @@ import streamlit as st
 import pandas as pd
 import datetime
 import db
-import yaml
  
 
   
-creation_dates=  datetime.datetime.now()
-creation_date_format = creation_dates.strftime('%Y%m%d%H%M%S')
-sysdate = creation_dates.strftime('%Y-%m-%d %H:%M:%S')
-effective_start= creation_dates.strftime('%Y-%m-%d')
-effective_end_dates = "4712-12-31"
-effective_end_date = datetime.datetime.strptime(effective_end_dates, "%Y-%m-%d").date()
-
 
 def main():
   st.header("Marching Band Dunia Fantasi")
