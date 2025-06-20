7a9b7ab2395858f4e81560e8b7049d6364cd6369
adaanaknya <yogyog377@gmail.com>
2025-06-20 10:16:05 +0700

    Update db.py

--- db.py
+++ db.py
@@ -6,17 +6,22 @@ import datetime
 from mysql.connector import Error
 import streamlit
 
-hostname = "0-igg.h.filess.io"
-database = "storage_costtownon"
-port = "61002"
-username = "storage_costtownon"
-password = "c06342c974f010f014d095af8ebf435cbfd5beca"
 
-# hostname = "sql.freedb.tech"
-# database = "freedb_stufandb"
-# port = "3306"
-# username = "freedb_stufan"
-# password = "9d2KQKRUJ4A!E@d"
+# Second backup
+# hostname = "0-igg.h.filess.io"
+# database = "storage_costtownon"
+# port = "61002"
+# username = "storage_costtownon"
+# password = "c06342c974f010f014d095af8ebf435cbfd5beca"
+
+
+
+# Primary
+hostname = "mysql.db.mdbgo.com"
+database = "mysql8"
+port = "3306"
+username = "adaanaknya_stufanorg"
+password = "Terserahluu123."
 
 # hostname = "sql.freedb.tech"
 # database = "freedb_stufandb"
