d16a3fd8ec648c090b8325f16c3b8f78f3c280de
adaanaknya <124648170+adaanaknya@users.noreply.github.com>
2025-06-22 17:29:53 +0700

    Update db.py

--- db.py
+++ db.py
@@ -8,20 +8,20 @@ import streamlit
 
 
 # Second backup
-# hostname = "0-igg.h.filess.io"
-# database = "storage_costtownon"
-# port = "61002"
-# username = "storage_costtownon"
-# password = "c06342c974f010f014d095af8ebf435cbfd5beca"
+ hostname = "0-igg.h.filess.io"
+ database = "storage_costtownon"
+ port = "61002"
+ username = "storage_costtownon"
+ password = "c06342c974f010f014d095af8ebf435cbfd5beca"
 
 
 
 # Primary
-hostname = "mysql.db.mdbgo.com"
-database = "mysql8"
-port = "3306"
-username = "adaanaknya_stufanorg"
-password = "Terserahluu123."
+#hostname = "mysql.db.mdbgo.com"
+#database = "mysql8"
+#port = "3306"
+#username = "adaanaknya_stufanorg"
+#password = "Terserahluu123."
 
 # hostname = "sql.freedb.tech"
 # database = "freedb_stufandb"
