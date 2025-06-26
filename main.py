af1251f7a315ac0093f8a0b4a7df023dc6a3f948
adaanaknya <yogyog377@gmail.com>
2025-06-26 10:05:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import pandas as pd
 import datetime
 import db
  
-user = st.experimental_user
+user = st.user
 def main():
 
 
   if user is None:
       st.warning("User belum terdeteksi. Coba login dulu ke https://streamlit.io/cloud lalu akses ulang app.")
   else:
-      st.success(f"Login sebagai: {user.email} ")
+      st.success(f"Login sebagai: {user} ")
       st.write(user)
   st.header("Marching Band Dunia Fantasi")
   st.image("stufan.jpg",caption="Stufan Dcorps")
