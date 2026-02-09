632314c2ae5a2ba14a643e54709dd625f0d70784
adaanaknya <124648170+adaanaknya@users.noreply.github.com>
2026-02-09 08:43:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import pandas as pd
 import datetime
 import db
  
-user = st.experimental_user
+ 
 def main():
 
 
-  if user is None:
-      st.warning("User belum terdeteksi. Coba login dulu ke https://streamlit.io/cloud lalu akses ulang app.")
-  else:
-        st.write("📧 Email:", getattr(user, "email", "Tidak ada"))
-        st.write("🆔 User ID:", getattr(user, "user_id", "Tidak ada"))
+   
   st.header("Marching Band Dunia Fantasi")
   st.image("stufan.jpg",caption="Stufan Dcorps")
  
 if __name__ == '__main__':
  
         main()
+
     
